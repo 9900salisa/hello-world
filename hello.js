a06a60d3c8212979ea7fda3067ee4ff9b504f559
Salisa <159696788+9900salisa@users.noreply.github.com>
2024-03-04 01:52:35 +0500

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,10 @@
+// console.log("hello world!")
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+// let Name:string = "salisa";
+// console.log("salisa")
+var personName = "fatima";
+console.log("hello, ".concat(personName, " whould you like to learn some python todat?"));
+var Name = "sila";
+console.log(Name.toLowerCase());
+console.log(Name.toUpperCase());
+console.log(Name.charAt(0).toUpperCase() + Name.slice(1).toLowerCase());
---
+++ hello.ts
@@ -0,0 +1,22 @@
+// console.log("hello world!")
+
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+// let Name:string = "salisa";
+
+// console.log("salisa")
+
+ let personName :string = "fatima"
+
+console.log(`hello, ${personName} whould you like to learn some python todat?`)
+
+ let Name: string = "sila"
+
+console.log(Name.toLowerCase());
+
+console.log(Name.toUpperCase());
+
+console.log(Name.charAt(0) . toUpperCase() + Name.slice(1).toLowerCase());
+
+
+
